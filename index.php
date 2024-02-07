f56f1fd0368e5a67e5f9629bd77ee9dae40e5c0e
David Agor <agordavid14@gmail.com>
2024-02-07 04:31:31 +0100

    first page

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quiz App</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header class="container">
+        <h1>PHP Quizzer</h1>
+    </header>
+
+    <main>
+        <div class="container">
+            <h2>Evaluation Quiz</h2>
+            <p>This is a multiple choice quiz to test your knowledge of programming</p>
+            <ul>
+                <li><strong>Number of Questions: </strong> </li>
+                <li><strong>Type: </strong>Multiple Choice </li>
+                <li><strong>Estimated Time: </strong>4 Minutes </li>
+            </ul>
+            <a href="question.php?n=1" class="start">Start Quiz</a>
+
+        </div>
+    </main>
+
+
+    <footer>
+        <div class="contianer">
+            <p>@2024</p>
+        </div>
+    </footer>
+</body>
+</html>
